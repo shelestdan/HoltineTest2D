2e923c6a91e51206ad6774f0ace6ee764db37f61
shelestdan <daniil.shelesteev@yandex.ru>
2022-09-08 12:59:02 +0300

    Fix

--- Assets/Scripts/BulletCol.cs
+++ Assets/Scripts/BulletCol.cs
@@ -3,12 +3,16 @@ using UnityEngine;
 
 public class BulletCol : MonoBehaviour
 {
-
-  public GameObject hitEffect;
-  private void OnCollisionEnter2D(Collision2D collision)
-  {
+    public GameObject hitEffect;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
       GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
       Destroy(effect, 1.5f);
       Destroy(gameObject);
-  }
+    }
+
+    private void Update()
+    {
+      Destroy(gameObject, 2.7f);
+    }
 }
